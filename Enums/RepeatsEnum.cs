f463ba8b33055df666218826aa3e6e5390a996ce
Erencan Pelin <erencan.pelin@gmail.com>
2023-01-18 11:02:23 +1100

    Add Entity Framework

---
+++ Enums/RepeatsEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetApp.Enums
+{
+    public enum RepeatsEnum
+    {
+        Yearly = 1,
+        Biannual = 2,
+        Quarterly = 4,
+        Monthly = 12,
+    }
+}
---
+++ MVVM/Model/Expense.cs
@@ -0,0 +1,15 @@
+﻿using BudgetApp.Enums;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BudgetApp.MVVM.Model
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; } //PK
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public RepeatsEnum Repeats { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; } = new ObservableCollection<Tag>();
+    }
+}
